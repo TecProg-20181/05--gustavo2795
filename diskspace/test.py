4606a85efbd4f4c86ca776d209171c97958b549b
gustavo2795 <gustavo.unb27@gmail.com>
2018-06-22 14:58:36 -0300

    creating subprocess_check_output_test and print_tree_test

--- diskspace/test.py
+++ diskspace/test.py
@@ -16,3 +16,18 @@ class DiskspaceTest(unittest.TestCase):
         blocks = 224
         result = "112.00Kb"
         self.assertEqual(bytes_to_readable(blocks), result)
+
+    def subprocess_check_output_test(self):
+        command = 'du'
+        du_result = subprocess.check_output(command)
+        result = subprocess_check_output(command)
+        self.assertEqual(du_result, result)
+
+    def print_tree_test(self):
+        cap = StringIO.StringIO()
+        sys.stdout = cap
+        print_tree(self.file_tree, self.file_tree_node, self.path,
+                   self.largest_size, self.total_size)
+        result = "2.00Kb  100%  /home/teste\n"
+        sys.stdout = sys.__stdout__
+        self.assertEqual(result, cap.getvalue())

4de5613175d42d37d7f8836a08b3628981065f4f
gustavo2795 <gustavo.unb27@gmail.com>
2018-06-22 14:53:26 -0300

    creating set_file_tree method

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,5 +1,13 @@
 import unittest
-+
-+from diskspace import *
-+
-+class test(unittest.TestCase):
+
+from diskspace import *
+
+class DiskspaceTest(unittest.TestCase):
+    
+    def set_file_tree(self):
+        self.file_tree = {'/home/teste': {'print_size': '2.00Kb', 'children': [], 'size': 4}}
+        self.file_tree_node = {'print_size': '2.00Kb',
+                               'children': [], 'size': 4}
+        self.path = '/home/teste'
+        self.largest_size = 6
+        self.total_size = 4
